d638d807debdf5b888070d697a3d57062dc92250
nitin <nitingojiya2000@gmail.com>
2025-07-04 12:15:14 +0530

    pdf preview and add more on preview page

--- app/javascript/controllers/pdf_upload_controller.js
+++ app/javascript/controllers/pdf_upload_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["fileInput", "selectButton", "buttonContainer","uploadContainer", "pdfmergeContainer"]
+  static targets = ["fileInput", "selectButton", "buttonContainer","uploadContainer", "pdfmergeContainer","previewContainer"]
 
   connect() {
     this.files = [] // Store files in memory
@@ -38,13 +38,41 @@ export default class extends Controller {
       const fileNames = this.files.map(f => f.name)
       console.log("file:", this.files);
       sessionStorage.setItem("selectedFileNames", JSON.stringify(fileNames))
+      this.filepreview();
     }
   }
 
-next() {
-  this.pdfmergeContainerTarget.style.display = 'initial'; // Show the PDF merge container
-  this.uploadContainerTarget.style.visibility = 'none';
-  this.buttonContainerTarget.style.visibility = 'hidden'; // Hide the button container
-  console.log("next button clicked");
+ next() {
+  this.pdfmergeContainerTarget.style.display = 'block';
+  this.uploadContainerTarget.style.display = 'none';
+  this.buttonContainerTarget.style.visibility = 'hidden';
+  this.filepreview();
+}
+
+  filepreview(){
+    this.previewContainerTarget.innerHTML = '';
+
+  this.files.forEach(file => {
+    const blobURL = URL.createObjectURL(file);
+
+    const previewCard = document.createElement('div');
+    previewCard.className = "flex flex-col items-center justify-center p-4 bg-green-50 border border-gray-200 rounded-lg shadow-sm  cursor-pointer mb-2";
+
+    previewCard.innerHTML = `
+      <div class="preview-thumbnail w-[300px] p-5 h-40 bg-gray-200 rounded-md mb-3 flex items-center justify-center text-gray-500 overflow-hidden">
+        <span class="text-sm text-gray-500">Click to preview PDF</span>
+      </div>
+      <p class="w-[300px] text-sm font-medium text-gray-700 text-center truncate px-2">${file.name}</p>
+    `;
+
+    // Add click behavior to show the PDF preview
+    previewCard.querySelector('.preview-thumbnail').addEventListener('click', function () {
+      this.innerHTML = `
+        <img src="${blobURL}" type="application/pdf" class="w-full h-full object-cover" />
+      `;
+    });
+
+    this.previewContainerTarget.appendChild(previewCard);
+  });
   }
 }
