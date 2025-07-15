56829bed202714926095acd4e7870ddc86472cc2
nitin <nitingojiya2000@gmail.com>
2025-07-15 13:00:05 +0530

    common js for pdf to jpg

--- app/javascript/controllers/pdf_to_word_controller.js
+++ app/javascript/controllers/pdf_to_word_controller.js
@@ -2,7 +2,6 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="pdf-to-word"
 export default class extends Controller {
-  static targets = ["fileInput", "selectButton", "buttonContainer", "uploadContainer", "pdfmergeContainer", "previewContainer"]
 
   connect() {
     this.files = [] // Store files in memory
